baa2f61672c1b5fcd383030940ee3e8537e01f1b
mditsolutions1 <101211536+mditsolutions1@users.noreply.github.com>
2023-12-29 18:42:36 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devops.mvc-game" # Replace with your actual S3 bucket name
+    bucket = "devops.mvc-project" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-southeast-2"
   }
